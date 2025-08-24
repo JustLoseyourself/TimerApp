5bdb1bec91ec01b303eee4dcf5129277da210443
Carlos Becerra <92628628+JustLoseyourself@users.noreply.github.com>
2025-08-24 22:15:59 +0200

    fix: updated version code for play store

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.justloseyourself.mytimerapp"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
+        versionCode = 4
         versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
